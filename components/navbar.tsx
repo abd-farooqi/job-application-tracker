1a4b59f2e9ddcc8578567c334bed4e7e189e4087
abd-farooqi <abdullahfarooqi332008@gmail.com>
2026-07-17 11:21:00 +0500

    feat: create schemas

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -39,18 +39,22 @@ export default function Navbar() {
               </Link>
               <DropdownMenu>
                 <DropdownMenuTrigger>
-                  <Avatar>
+                  <Avatar className="h-8 w-8">
                     <AvatarFallback className="bg-primary text-white">
                       {session.user.name[0].toUpperCase()}
                     </AvatarFallback>
                   </Avatar>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent className="min-w-56">
+                <DropdownMenuContent className="min-w-56" align="end">
                   <DropdownMenuGroup>
-                    <DropdownMenuLabel>
-                      <div>
-                        <p>{session.user.name}</p>
-                        <p>{session.user.email}</p>
+                    <DropdownMenuLabel className="font-normal">
+                      <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium leading-none">
+                          {session.user.name}
+                        </p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                          {session.user.email}
+                        </p>
                       </div>
                     </DropdownMenuLabel>
                     <SignOut />
